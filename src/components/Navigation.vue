27e3c5927a9be24e91774ef3fe3f45125965efd6
Jeff Mills <blatant3@gmail.com>
2023-02-07 17:20:21 -0500

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -14,32 +14,31 @@
 
 </script>
 <template>
-    
-    <div class="max-w-screen-xl mx-auto p-6 flex items-center justify-between w-full text-white">
 
-        <div class="w-1/3 text-left">SITE NAME</div>
-        
-        <nav class="w-1/3 flex items-center justify-center gap-10 text-white">
-            <div v-for="navItem in navigationItems" :key="navItem.label">
+    <div class="sticky top-0 z-50 bg-black shadow-md">
+    
+        <div class="max-w-screen-xl mx-auto p-6 flex items-center justify-between w-full text-white">
+
+            <div class="w-1/3 text-left">SITE NAME</div>
+            
+            <nav class="w-1/3 flex items-center justify-center gap-10 text-white">
+                <div v-for="navItem in navigationItems" :key="navItem.label">
+                    <router-link
+                        :to="navItem.destination"
+                        class="transition hover:text-blue-500">
+                        {{ navItem.label }}
+                    </router-link>
+                </div>
+            </nav>
+            
+            <div class="w-1/3 text-right">
                 <router-link
-                    :to="navItem.destination"
-                    class="transition hover:text-blue-500">
-                    {{ navItem.label }}
+                    :to="postJobItem.destination"
+                    class="font-bold py-2 px-6 rounded-full bg-[color:var(--p-orange)]">
+                    {{ postJobItem.label }}
                 </router-link>
             </div>
-        </nav>
-        
-        <div class="w-1/3 text-right">
-            <!-- <router-link
-                :to="postJobItem.destination"
-                class="font-bold py-2 px-6 rounded-full bg-gradient-to-r from-blue-500 to-blue-600">
-                {{ postJobItem.label }}
-            </router-link> -->
-            <router-link
-                :to="postJobItem.destination"
-                class="font-bold py-2 px-6 rounded-full bg-[color:var(--p-orange)]">
-                {{ postJobItem.label }}
-            </router-link>
         </div>
+        
     </div>
 </template>
