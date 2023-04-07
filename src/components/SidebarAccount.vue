d75c1682226a3ab23e085973241129f6572c5edd
Jeff Mills <blatant3@gmail.com>
2023-04-07 16:00:31 -0400

    Update SidebarAccount.vue

--- src/components/SidebarAccount.vue
+++ src/components/SidebarAccount.vue
@@ -1,42 +1,24 @@
 <script setup>
     import { useRoute } from 'vue-router'
     const route = useRoute()
+
+    const links = [
+        { label: 'Account Details', routeName: 'account' },
+        { label: 'Manage Listings', routeName: 'my-listings' },
+        { label: 'Manage Companies', routeName: 'my-companies' },
+        { label: 'Post a Job', routeName: 'post-job-details' },
+    ]
 </script>
 <template>
     <aside class="w-1/4">
-
-        <div class="bg-white w-full shadow-md text-gray-800 rounded-lg">
-            
+        <div class="w-full text-gray-800 rounded-lg">
             <ul>
-                
-                <li
-                    class="px-4 py-2 flex items-center justify-between border-b border-gray-200 rounded-t-lg"
-                    :class="{ 'bg-gray-200' : route.name == 'account' }"
-                >
-                    <router-link to="/account" class="font-semibold">Account Details</router-link>
-                    <font-awesome-icon icon="fa-solid fa-chevron-right" />
-                </li>
-                
-                <li
-                    class="px-4 py-2 flex items-center justify-between border-b border-gray-200"
-                    :class="{ 'bg-gray-200' : route.name == 'edit-listing' || route.name == 'my-listings' }"
-                >
-
-                    <router-link to="/account/listings" class="font-semibold">Manage Listings</router-link>
-                    <font-awesome-icon icon="fa-solid fa-chevron-right" />
-                </li>
-                
-                <li
-                    class="px-4 py-2 flex items-center justify-between border-b border-gray-200"
-                    :class="{ 'bg-gray-200' : route.name == 'my-companies' || route.name == 'edit-company' }"
-                >
-                    <router-link to="/account/companies" class="font-semibold">Manage Companies</router-link>
-                    <font-awesome-icon icon="fa-solid fa-chevron-right" />
-                </li>
-                
-                <li class="px-4 py-2 flex items-center justify-between">
-                    <router-link :to="{ name: 'post-job-details' }" class="font-semibold">Post a Job</router-link>
-                    <font-awesome-icon icon="fa-solid fa-chevron-right" />
+                <li v-for="link in links" :key="link">
+                    <router-link
+                        :to="{ name: link.routeName }"
+                        class="font-semibold block p-2 w-full hover:bg-white rounded-lg">
+                        {{ link.label }}
+                    </router-link>
                 </li>
             </ul>
         </div>
